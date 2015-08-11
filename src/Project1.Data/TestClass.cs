65a7bb3995b8525699f598b612ee932ea2650afc
Radenko Zec <logic.rabbit@gmail.com>
2015-08-11 09:58:40 +0200

    Initial commit

---
+++ src/Project1.Data/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Data
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class TestClass
+    {
+        public void Test()
+        {
+            using (SqlConnection conn = new SqlConnection(""))
+            {
+                
+            }
+        }
+    }
+}
---
+++ src/Tests.Integration/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project1.Data;
+using Xunit;
+
+namespace Tests.Integration
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            TestClass testClass = new TestClass();
+            testClass.Test();
+            Assert.True(true);
+        }
+    }
+}
